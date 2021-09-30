2b0b286d1b4167895e52126c4e27c390d521d0b9
ptn1411 <57529765+ptn1411@users.noreply.github.com>
2021-09-30 12:07:27 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,14 @@
+'use strict'
+
+module.exports = {
+  mailer: {
+    service: 'Gmail',
+    auth: {
+      user: 'phamthanhnamdev@gmail.com',
+      pass: 'nguyenxuantrung2'
+    }
+  },
+  dbConnstring: 'mongodb://127.0.0.1:27017/codeshare',
+  sessionKey: 'codeshare'
+}
+
